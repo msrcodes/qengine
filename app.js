1a9261a4cf2f0e0c7cacc84ec31114a74099b940
mikaelsrozee <mikaelsrozee@gmail.com>
2020-02-13 11:49:16 +0000

    display fetched array of questions from the server to the client

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ const app = express();
 
 app.use(express.static('client', {extensions: ['html']}));
 
+function getQuestions(req, res) {
+    const questions = ["Is this a test question?", "Is Shrek green?", "Who is Joe?"]; // TODO: Move this to question.js
+    res.json(questions);
+}
+
+app.get('/questions', (req, res) => getQuestions(req, res));
+
 app.listen(8080);
--- client/index.js
+++ client/index.js
@@ -2,12 +2,40 @@
 
 const pageElements = {};
 
+function removeChildren(elem) {
+    while (elem.firstElementChild) {
+        elem.remove(elem.firstElementChild);
+    }
+}
+
+function populateList(list, data) {
+    for (const i of data) {
+        const child = document.createElement("li");
+        child.textContent = i;
+        list.append(child);
+    }
+}
+
+async function displayQuestions() {
+    const response = await fetch('questions');
+    let questionsObj;
+    if (response.ok) {
+        questionsObj = await response.json();
+    } else {
+        questionsObj = ["Error; could not load questions."]; // TODO: proper error handling
+    }
+
+    removeChildren(pageElements.questions);
+    populateList(pageElements.questions, questionsObj);
+}
+
 function getHandles() {
     pageElements.questions = document.querySelector("#questions");
 }
 
 function onPageLoad() {
     getHandles();
+    displayQuestions();
 }
 
 window.addEventListener('load', onPageLoad);
