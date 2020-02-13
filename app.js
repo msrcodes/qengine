9c3d626224872da15137096dc169ed067b60271d
mikaelsrozee <mikaelsrozee@gmail.com>
2020-02-13 21:31:02 +0000

    use questionnaire object instead of questions array

--- app.js
+++ app.js
@@ -5,55 +5,60 @@ const app = express();
 
 app.use(express.static('client', {extensions: ['html']}));
 
-let questions = [
+let questionnaires = [
     {
-        "id": "name",
-        "text": "What is your name?",
-        "type": "text"
-    },
-    {
-        "id": "quest",
-        "text": "What is your quest?",
-        "type": "text"
-    },
-    {
-        "id": "col",
-        "text": "What is your favourite colour?",
-        "type": "text"
-    },
-    {
-        "id": "velo",
-        "text": "What is the air-speed velocity of an unladen swallow?",
-        "type": "number"
-    },
-    {
-        "id": "lord",
-        "text": "Which is the best lord?",
-        "type": "single-select",
-        "options": [
-            "Lord of the Rings",
-            "Lord of the Flies",
-            "Lord of the Dance",
-            "Lorde"
+        "name": "Example Questionnaire",
+        "questions": [
+            {
+                "id": "name",
+                "text": "What is your name?",
+                "type": "text"
+            },
+            {
+                "id": "quest",
+                "text": "What is your quest?",
+                "type": "text"
+            },
+            {
+                "id": "col",
+                "text": "What is your favourite colour?",
+                "type": "text"
+            },
+            {
+                "id": "velo",
+                "text": "What is the air-speed velocity of an unladen swallow?",
+                "type": "number"
+            },
+            {
+                "id": "lord",
+                "text": "Which is the best lord?",
+                "type": "single-select",
+                "options": [
+                    "Lord of the Rings",
+                    "Lord of the Flies",
+                    "Lord of the Dance",
+                    "Lorde"
+                ]
+            },
+            {
+                "id": "langs",
+                "text": "Which computer languages have you used?",
+                "type": "multi-select",
+                "options": [
+                    "JavaScript",
+                    "Java",
+                    "C",
+                    "Python",
+                    "Ook",
+                    "LISP"
+                ]
+            }
         ]
     },
-    {
-        "id": "langs",
-        "text": "Which computer languages have you used?",
-        "type": "multi-select",
-        "options": [
-            "JavaScript",
-            "Java",
-            "C",
-            "Python",
-            "Ook",
-            "LISP"
-        ]
-    }
 ]; // TODO: Move this to question.js
 
 function getQuestions(req, res) {
-    res.json(questions);
+    res.json(questionnaires[0].questions);
 }
 
 app.get('/questions', (req, res) => getQuestions(req, res));
@@ -65,8 +70,8 @@ app.post('/questions', express.json(), (req, res) => {
         type: req.body.type,
         options: req.body.options,
     };
-    questions = [question, ...questions];
-    res.json(questions);
+    questionnaires[0].questions = [question, ...questionnaires[0].questions];
+    res.json(questionnaires[0].questions);
 });
 
 app.listen(8080);
