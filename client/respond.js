de121ff6c64a6b5c4b342fe0ae53b9893ae90fb8
mikaelsrozee <mikaelsrozee@gmail.com>
2020-02-20 11:52:44 +0000

    create template questionnaire

---
+++ client/respond.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const pageElements = {};
+
+function addEventListeners() {
+
+}
+
+function getHandles() {
+
+}
+
+function initPage() {
+
+}
+
+function onPageLoad() {
+    getHandles();
+    addEventListeners();
+    initPage();
+}
+
+window.addEventListener('load', onPageLoad);
