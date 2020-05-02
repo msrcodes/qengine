a3211bc348093e9694a3e17bca4b8f0504ac4eb8
Mikael Rozee <mikaelsrozee@gmail.com>
2020-05-02 12:29:54 +0100

    change single-select and multi-select response display format to be more useful

--- client/responses.js
+++ client/responses.js
@@ -39,6 +39,24 @@ function addResponseContainer(question) {
     const template = document.querySelector("#response");
     const clone = template.content.cloneNode(true);
 
+    if (question.type === "single-select" || question.type === "multi-select") {
+        clone.querySelector(".responses").dataset.type = "select";
+
+        for (const option of question.options) {
+            const li = document.createElement("li");
+            li.dataset.option = option;
+            li.append(document.createTextNode(`${option}: `));
+
+            const span = document.createElement("span");
+            span.append(document.createTextNode("0"));
+            li.append(span);
+
+            li.dataset.id = JSON.stringify([]);
+
+            clone.querySelector(".responses").append(li);
+        }
+    }
+
     clone.querySelector(".response-title").textContent = question.text;
     clone.querySelector(".responses").dataset.id = question.id;
 
@@ -46,33 +64,67 @@ function addResponseContainer(question) {
 }
 
 function highlightResponses(event) {
-    const likeResponses = document.querySelectorAll(`[data-id~="${event.target.dataset.id}"]`);
+    const ids = JSON.parse(event.target.dataset.id);
+    const allResponses = document.querySelectorAll(".responses > li");
 
     if (event.type === "mouseenter") {
-        for (const response of likeResponses) {
-            response.style = "font-weight: bold;";  // TODO: use CSS classes instead
+        for (const id of ids) {
+            for (const response of allResponses) {
+                const resIds = JSON.parse(response.dataset.id);
+                if (resIds.includes(id)) {
+                    response.style = "font-weight: bold;"; // TODO: use CSS classes instead
+                }
+            }
         }
     } else if (event.type === "mouseout") {
-        for (const response of likeResponses) {
-            response.style = "";    // TODO: use CSS classes instead
+        for (const id of ids) {
+            for (const response of allResponses) {
+                const resIds = JSON.parse(response.dataset.id);
+                if (resIds.includes(id)) {
+                    response.style = "";
+                }
+            }
         }
     }
+}
+
+function updateSelectCount(li, i) {
+    const array = JSON.parse(`${li.dataset.id}`);
+    array.push(i);
+    li.dataset.id = JSON.stringify(array);
+
+    const span = li.querySelector("span");
+    span.textContent = String(Number(span.textContent) + 1);
 }
 
 function addResponse(response, i) {
     for (const key of Object.keys(response)) {
         const ul = document.querySelector(`[data-id~="${key}"]`);
-        const li = document.createElement("li");
 
         if (ul == null) // If the response contains a question that the questionnaire does not, skip
             continue;
 
-        li.appendChild(document.createTextNode(response[key]));
-        li.dataset.id = i;
-        ul.appendChild(li);
+        if (ul.dataset.type === "select") {
+            const ans = response[key];
+            if (typeof ans === "object") {
+                for (const elem of ans) {
+                    const li = ul.querySelector(`[data-option~="${elem}"]`);
+                    updateSelectCount(li, i);
+                }
+            } else {
+                const li = ul.querySelector(`[data-option~="${response[key]}"]`);
+                updateSelectCount(li, i)
+            }
+        } else {
+            const li = document.createElement("li");
 
-        li.addEventListener('mouseenter', highlightResponses);
-        li.addEventListener('mouseout', highlightResponses);
+            li.appendChild(document.createTextNode(response[key]));
+            li.dataset.id = JSON.stringify([i]);
+            ul.appendChild(li);
+
+            li.addEventListener('mouseenter', highlightResponses);
+            li.addEventListener('mouseout', highlightResponses);
+        }
     }
 }
 
@@ -86,6 +138,7 @@ async function initPage() {
     }
 
     const responses = await getResponses();
+
     let i = 0;
     for (const response of responses) {
         addResponse(response, i);
