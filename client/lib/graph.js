08837c62befa6b34dbe2fcf3df50e9c472db540b
Mikael Rozee <mikaelsrozee@gmail.com>
2020-05-29 16:48:29 +0100

    label graphs

--- client/lib/graph.js
+++ client/lib/graph.js
@@ -55,8 +55,9 @@ export class Graph extends HTMLElement {
             // establish variables
             const keys = Object.keys(data);
             const axisSize = spacing / 2;
-            const barWidth = (canvas.width - (spacing * (keys.length - 1)) - axisSize) / keys.length;
-            const scaleFactor = this.calculateScaleFactor(data, canvas.height - axisSize);
+            const offset = 20; // used for gap between axis and edge for labels
+            const barWidth = (canvas.width - (spacing * (keys.length - 1)) - axisSize - offset) / keys.length;
+            const scaleFactor = this.calculateScaleFactor(data, canvas.height - axisSize - offset - offset);
             const colour = `255, 51, 0`;
             let alpha = 1;
 
@@ -72,22 +73,55 @@ export class Graph extends HTMLElement {
 
             // draw axis
             ctx.fillStyle = "#000000";
-            ctx.fillRect(0, 0, axisSize, canvas.height);
-            ctx.fillRect(0, 0, canvas.width, axisSize);
+            ctx.fillRect(offset, offset, axisSize, canvas.height - offset - offset);
+            ctx.fillRect(offset, offset, canvas.width - offset, axisSize);
 
             // draw bars
             ctx.fillStyle = `rgba(${colour}, ${alpha})`;
 
             let x = 0;
+            const values = [];
             for (const key of keys) {
-                ctx.fillRect(x + axisSize, axisSize, barWidth, data[key] * scaleFactor);
+                const value = data[key];
+                values.push(value);
+                ctx.fillRect(x + axisSize + offset, axisSize + offset, barWidth, value * scaleFactor);
+
+                // label x-axis
+                const c = ctx.getTransform();
+                ctx.resetTransform();
+                ctx.fillStyle = "#000000";
+                ctx.fillText(key, x + axisSize + offset + (barWidth / 2), canvas.height - (offset / 2), barWidth);
+                ctx.setTransform(c);
+
                 ctx.fillStyle = `rgba(${colour}, ${alpha})`;
 
                 x += spacing + barWidth;
                 alpha -= (1 / keys.length);
             }
 
-            // reset context
+            ctx.resetTransform();
+
+            // label y-axis
+            ctx.fillStyle = "#000000";
+            ctx.textAlign = "center";
+            let val = Math.max(...values);
+
+            const getGap = (val) => {
+                if (val > 100) return 10;
+                else if (val > 50) return 5;
+                else if (val > 25) return 3;
+                else if (val > 10) return 2;
+                else return 1;
+            };
+            const gap = getGap(val);
+
+            const y = (y) => {return canvas.height - (y * scaleFactor) - offset};
+            while (val >= 0) {
+                ctx.fillText(String(val), offset / 2, y(val));
+
+                val -= gap;
+            }
+
             ctx.resetTransform();
         } else {
             // TODO: canvas-unsupported code here
